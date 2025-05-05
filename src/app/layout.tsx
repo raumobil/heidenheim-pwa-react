7e32fd0972d00d638ac17d3d4f38d9ebe942259f
Philipp Bohnenstengel <p.bohnenstengel@raumobil.com>
2025-05-05 16:18:17 +0200

    build: install and configure nextjs integration for material-ui
    https://mui.com/material-ui/integrations/nextjs/#app-router
    - `npm install @mui/material-nextjs @emotion/cache`
    - add `AppRouterCacheProvider` to `layout.tsx`
    - configure theme and enable css variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Roboto } from "next/font/google"
+import "./globals.css"
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
+import { ThemeProvider } from "@mui/material"
+import theme from "@/theme"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,13 +20,17 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={roboto.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
-  );
+  )
 }
