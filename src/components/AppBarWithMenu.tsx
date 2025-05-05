37ffc3bafe2803bdbbeffba11aa95239eb02d418
Philipp Bohnenstengel <p.bohnenstengel@raumobil.com>
2025-05-05 18:24:42 +0200

    feat: add Drawer to AppBarWithMenu

--- src/components/AppBarWithMenu.tsx
+++ src/components/AppBarWithMenu.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { Menu } from "@mui/icons-material";
-import { AppBar, Grid, IconButton, styled, Toolbar } from "@mui/material";
+import { Close, Menu } from "@mui/icons-material";
+import { AppBar, Drawer, Grid, IconButton, List, ListItem, ListItemButton, ListItemText, styled, Toolbar, Typography } from "@mui/material";
+import { useCallback, useState } from "react";
 
 // see https://mui.com/material-ui/react-app-bar/#fixed-placement
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar)
@@ -9,30 +10,84 @@ const Offset = styled('div')(({ theme }) => theme.mixins.toolbar)
 /**
  * this component contains the main navigation
  */
-const AppBarWithMenu = () => (
-  <>
-    <AppBar
-      position='fixed'
-      component='nav'
-      sx={{
-        backgroundColor: 'background.paper'
-      }}
-    >
-      <Toolbar>
-        <Grid container direction='row' size='grow'>
-          <Grid size='grow'>
-            {/* placeholder for logo */}
+const AppBarWithMenu = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const openMenu = useCallback(() => {
+    setMenuOpen(true)
+  }, [])
+
+  const closeMenu = useCallback(() => {
+    setMenuOpen(false)
+  }, [])
+  
+  return (
+    <>
+      <AppBar
+        position='fixed'
+        component='nav'
+        sx={{
+          backgroundColor: 'background.paper'
+        }}
+      >
+        <Toolbar>
+          <Grid container direction='row' size='grow'>
+            <Grid size='grow'>
+              {/* placeholder for logo */}
+            </Grid>
+            <Grid size='auto'>
+              <IconButton onClick={openMenu}>
+                <Menu color='primary' />
+              </IconButton>
+            </Grid>
           </Grid>
-          <Grid size='auto'>
-            <IconButton>
-              <Menu color='primary' />
-            </IconButton>
+        </Toolbar>
+        <Drawer
+          anchor='right'
+          open={menuOpen}
+          onClose={closeMenu}
+          ModalProps={{
+            keepMounted: true
+          }}
+          slotProps={{
+            paper: {
+              sx: {
+                width: '100%'
+              }
+            }
+          }}
+        >
+          <Grid container direction='column'>
+            <Grid>
+              <Toolbar>
+                <Grid container direction='row' alignItems='center' size='grow'>
+                  <Grid size='grow' justifyItems='center'>
+                    <Typography>Menü</Typography>
+                  </Grid>
+                  <Grid size='auto'>
+                    <IconButton onClick={closeMenu}>
+                      <Close/>
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </Toolbar>
+            </Grid>
+            <Grid>
+              <List>
+                <ListItem>
+                  <ListItemButton>
+                    <ListItemText primary='Kartenansicht' />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Grid>
           </Grid>
-        </Grid>
-      </Toolbar>
-    </AppBar>
-    <Offset />
-  </>
-)
+        </Drawer>
+      </AppBar>
+      <Offset />
+    </>
+  )
+}
 
 export default AppBarWithMenu
