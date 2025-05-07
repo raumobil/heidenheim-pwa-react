7321c19e05c61b1c6a1ef370c994360ffeacb823
Philipp Bohnenstengel <p.bohnenstengel@raumobil.com>
2025-05-07 19:12:26 +0200

    feat: dynamically generate page text from slug + i18n

--- src/app/[locale]/(withContainer)/[slug]/page.tsx
+++ src/app/[locale]/(withContainer)/[slug]/page.tsx
@@ -1,18 +1,45 @@
 import { Typography } from "@mui/material"
+import { getMessages, getTranslations } from "next-intl/server"
+import { notFound } from "next/navigation";
+import { Fragment } from "react";
 
 /**
  * this is a regular page with content depending on the slug (WIP)
  */
 const Page = async ({ params } : { params: Promise<{ slug: string}>}) => {
   const { slug } = await params
+
+  // load messages depending on slug
+  const messages = await getMessages();
+  const messagesForSlug = messages.pages?.[slug]
+  
+  // 404 if no messages found for slug
+  if (!messagesForSlug) {
+    notFound()
+  }
+
+  const t = await getTranslations(`pages.${slug}`)
   return (
-    <Typography
-      variant='titleExtraLarge'
-      component='h2'
-      color='textDark'
-    >
-      {slug}
-    </Typography>
+    <>
+      <Typography
+        variant='titleExtraLarge'
+        component='h2'
+        color='textDark'
+      >
+        {t('title')}
+      </Typography>
+      { // render typography for every key under `body`
+        messagesForSlug.body && Object.keys(messagesForSlug.body).map((key) => {
+        return (
+          <Fragment key={key}>
+            {t.rich(`body.${key}`, {
+              h3: (chunks) => <Typography variant='titleSmall' component='h3' marginTop={2} marginBottom={1} color='textDark'>{chunks}</Typography>,
+              p: (chunks) => <Typography variant='textLarge' component='p' color='textDark'>{chunks}</Typography>,
+            })}
+          </Fragment>
+        )
+      })}
+    </>
   )
 }
 
