0b690a0fd0e2d54280e9e79f43bff5aabd408e4f
paulKopf <p.kopf@raumobil.com>
2026-02-26 16:50:06 +0100

    chore(pwa-installation): add setOpen logic to new dialog
    Story: https://raumobil.atlassian.net/browse/HDN-277
    Sub-task: https://raumobil.atlassian.net/browse/HDN-279

--- src/components/PwaInstallationDialog.tsx
+++ src/components/PwaInstallationDialog.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import useMatomo from '@/components/Matomo/useMatomo'
 import { PwaContext } from '@/components/PwaContentProvider'
 import PwaInstallationButtonChromium from '@/components/PwaInstallationButtonChromium'
 import PwaInstallationInstructionIos from '@/components/PwaInstallationInstructionIos'
@@ -17,7 +18,7 @@ import {
   Typography,
 } from '@mui/material'
 import { useTranslations } from 'next-intl'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import InlineSVG from 'react-inlinesvg'
 
 /**
@@ -26,84 +27,96 @@ import InlineSVG from 'react-inlinesvg'
 const PwaInstallationDialog = () => {
   const { handleInstallClick, showInstallButton, showInstallationInstruction } =
     useContext(PwaContext)
+  const [open, setOpen] = useState(true)
+  const { trackEvent } = useMatomo()
 
   const t = useTranslations('PWAInstallation')
 
   return (
-    <Dialog
-      open={true}
-      sx={() => ({
-        '& .MuiDialog-paper': {
-          borderRadius: 2,
-        },
-      })}
-      aria-labelledby='pwa-installation-dialog-title'
-    >
-      <DialogTitle
-        id='pwa-installation-dialog-title'
-        variant='titleSmall'
-        sx={() => ({ '&.MuiDialogTitle-root': { p: 1.5, pb: 1 } })}
-      >
-        <Grid
-          container
-          direction={'row'}
-          flexWrap={'nowrap'}
-          gap={2}
-          alignContent={'center'}
+    <>
+      {open && (showInstallButton || showInstallationInstruction) && (
+        <Dialog
+          open={open}
+          onClose={() => {
+            trackEvent('PwaInstallationDialog', 'close', 'other')
+            setOpen(false)
+          }}
+          sx={() => ({
+            '& .MuiDialog-paper': {
+              borderRadius: 2,
+            },
+          })}
+          aria-labelledby='pwa-installation-dialog-title'
         >
-          <Grid flexGrow={0}>
-            <Avatar
-              variant={showInstallationInstruction ? 'rounded' : 'circular'}
+          <DialogTitle
+            id='pwa-installation-dialog-title'
+            variant='titleSmall'
+            sx={() => ({ '&.MuiDialogTitle-root': { p: 1.5, pb: 1 } })}
+          >
+            <Grid
+              container
+              direction={'row'}
+              flexWrap={'nowrap'}
+              gap={2}
+              alignContent={'center'}
             >
-              <SvgIcon viewBox='0 0 40 40'>
-                <InlineSVG width={40} height={40} src={'icon.svg'} />
-              </SvgIcon>
-            </Avatar>
-          </Grid>
-          <Grid alignItems={'flex-start'} flexGrow={0}>
-            {t('dialog.title')}
-          </Grid>
-        </Grid>
-      </DialogTitle>
-      <IconButton
-        aria-label='close'
-        // onClick={() => {
-        //   trackEvent('PWaInstallationDialog', 'close', 'button')
-        //   setOpen(false)
-        // }}
-        sx={{
-          position: 'absolute',
-          right: 12, // align with checkboxes
-          top: 8, // align with title
-        }}
-      >
-        <Close />
-      </IconButton>
-      <Divider sx={{ my: 1, py: 0 }} />
-      <DialogContent sx={{ px: 0, py: 1.5 }}>
-        <Grid container>
+              <Grid flexGrow={0}>
+                <Avatar
+                  variant={showInstallationInstruction ? 'rounded' : 'circular'}
+                >
+                  <SvgIcon viewBox='0 0 40 40'>
+                    <InlineSVG width={40} height={40} src={'icon.svg'} />
+                  </SvgIcon>
+                </Avatar>
+              </Grid>
+              <Grid alignItems={'flex-start'} flexGrow={0}>
+                {t('dialog.title')}
+              </Grid>
+            </Grid>
+          </DialogTitle>
+          <IconButton
+            aria-label='close'
+            onClick={() => {
+              trackEvent('PWaInstallationDialog', 'close', 'button')
+              setOpen(false)
+            }}
+            sx={{
+              position: 'absolute',
+              right: 12, // align with checkboxes
+              top: 8, // align with title
+            }}
+          >
+            <Close />
+          </IconButton>
+          <Divider sx={{ my: 1, py: 0 }} />
+          <DialogContent sx={{ px: 0, py: 1.5 }}>
+            <Grid container>
+              {showInstallButton && (
+                <Grid container justifyContent={'center'} px={2} gap={1.5}>
+                  <Typography variant='textLarge'>
+                    {t('dialog.chromiumText')}
+                  </Typography>
+                </Grid>
+              )}
+              {showInstallationInstruction && (
+                <Grid px={2} container direction={'row'}>
+                  <Typography variant='textLarge'>
+                    {t('dialog.iosText')}
+                  </Typography>
+                  <PwaInstallationInstructionIos />
+                </Grid>
+              )}
+            </Grid>
+          </DialogContent>
           {showInstallButton && (
-            <Grid container justifyContent={'center'} px={2} gap={1.5}>
-              <Typography variant='textLarge'>
-                {t('dialog.chromiumText')}
-              </Typography>
-            </Grid>
+            <DialogActions sx={{ justifyContent: 'center' }}>
+              {/* @ts-expect-error ignore undefined */}
+              <PwaInstallationButtonChromium onClick={handleInstallClick} />
+            </DialogActions>
           )}
-          {showInstallationInstruction && (
-            <Grid px={2} container direction={'row'}>
-              <Typography variant='textLarge'>{t('dialog.iosText')}</Typography>
-              <PwaInstallationInstructionIos />
-            </Grid>
-          )}
-        </Grid>
-      </DialogContent>
-      {showInstallButton && (
-        <DialogActions sx={{ justifyContent: 'center' }}>
-          {/* @ts-expect-error ignore undefined */}
-          <PwaInstallationButtonChromium onClick={handleInstallClick} />
-        </DialogActions>
+        </Dialog>
       )}
-    </Dialog>
+    </>
   )
 }
 
